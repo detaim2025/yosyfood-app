3dc599d48d62d1686341d9b886dfbe157c2924d6
detaim2025 <jorgecastro8040@gmail.com>
2025-10-27 01:55:04 -0500

    Trigger new deploy

--- app.py
+++ app.py
@@ -557,3 +557,4 @@ def consumo_eliminar(consumo_id):
 # -----------------------------------------------------
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+# Forzar re-despliegue 2
